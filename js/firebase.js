3db423a5a9ba4515737adf882e0c0d0f4430dfbd
Tristan Berger <tristan.berger6@gmail.com>
2022-01-13 17:52:17 +0100

    contact, footer, fr website

--- js/firebase.js
+++ js/firebase.js
@@ -34,12 +34,12 @@ function submitForm(e) {
   saveMessage(name, email, message);
 
   // Show alert
-  //document.querySelector('.alert').style.display = 'block';
+  document.querySelector('.alert').style.display = 'grid';
 
   // Hide alert after 3 seconds
-  //setTimeout(function () {
-  //  document.querySelector('.alert').style.display = 'none';
-  //}, 3000);
+  setTimeout(function () {
+    document.querySelector('.alert').style.display = 'none';
+  }, 3000);
 
   // Clear form
   document.getElementById('contactForm').reset();
